4987df4f4306eb75f467ccfae3818d72b589da69
Kir Proto Q <kir.proto@ya.ru>
2022-02-09 12:49:05 +0500

    Initial Commit

---
+++ Yan_SwiftUI_ViewList_and_NavigationView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Yan_SwiftUI_ViewList_and_NavigationView
+//
+//  Created by KIR Q on 2022.02.09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Yan_SwiftUI_ViewList_and_NavigationView/Yan_SwiftUI_ViewList_and_NavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_ViewList_and_NavigationViewApp.swift
+//  Yan_SwiftUI_ViewList_and_NavigationView
+//
+//  Created by KIR Q on 2022.02.09.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_ViewList_and_NavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
